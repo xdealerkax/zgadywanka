f4e4b3b3447bdf1f8eb8622d5c4dcfcf44e8b221
julka264 <61200823+julka264@users.noreply.github.com>
2020-02-20 19:56:19 +0100

    WERSJA 2

---
+++ Zgadywanka/ConsoleApp1 zgadywanie/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1_zgadywanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Gra za dużo za mało");
+
+            //1. kompyter losuje
+            #region losowanie
+            var los = new Random();   // nazwa typu nazwa zmiennej zmienna los typu random tworze obiekt typu random
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana);
+#endif
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100.\nOdgadnij ją!");
+            #endregion
+
+            bool odgadniete = false;
+            //dopóki nie odgadniete
+            while (! odgadniete )
+            {
+                // 2. czlowiek proponuje
+                Console.Write("Podaj swoją propozycję ");
+                int propozycja = int.Parse(Console.ReadLine());
+
+                //3. komputer ocenia 
+                if (propozycja < wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za mało");
+                    Console.ResetColor();
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Za dużo");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Trafiono");
+                    Console.ResetColor();
+                    odgadniete = true;
+
+                }
+            }
+            Console.WriteLine("Koniec gry");
+        } 
+    }
+}
